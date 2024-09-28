0311254096e6fc22619b2de62e75f5a0ecc7e8fd
johnlomat <edwardlomat1503@gmail.com>
2024-09-28 18:52:39 +0800

    Added newsletter form

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import NewsLetterForm from '@/components/forms/NewsLetter'
 
 const Footer = () => {
-  return <React.Fragment></React.Fragment>
+  return (
+    <React.Fragment>
+      <footer className="relative bg-white py-16">
+        <div className="container">
+          <div className="space-y-6 bg-brown p-16">
+            <div className="header-2 uppercase text-white">
+              <h2>Newsletter</h2>
+            </div>
+            <NewsLetterForm />
+          </div>
+        </div>
+      </footer>
+    </React.Fragment>
+  )
 }
 
 export default Footer
