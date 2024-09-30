623752876b253e31279b019acb7d32ae9ebf4d54
johnlomat <edwardlomat1503@gmail.com>
2024-09-30 15:01:59 +0800

    Fix HTML button being children of a button

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -62,7 +62,7 @@ const SheetContent = React.forwardRef<
     <SheetOverlay />
     <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-neutral-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-neutral-100">
-        <Button size="icon">
+        <Button size="icon" asChild>
           <Cross2Icon className="h-4 w-4" />
         </Button>
         <span className="sr-only">Close</span>
