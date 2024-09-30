cd3aa8b0a878560d5ab6b1af14e98bdfab2b3f61
johnlomat <edwardlomat1503@gmail.com>
2024-09-30 14:41:32 +0800

    Fix react animated cursor showing in mobile device

--- src/components/AnimatedCursor.tsx
+++ src/components/AnimatedCursor.tsx
@@ -2,9 +2,14 @@
 
 // Import with next's dynamic import
 import dynamic from 'next/dynamic'
+import { getDeviceType } from '@/lib/getDeviceType'
 
-const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
-  ssr: false,
-})
+const AnimatedCursor = dynamic(
+  () =>
+    getDeviceType() === 'Desktop'
+      ? import('react-animated-cursor')
+      : Promise.resolve({ default: () => null }),
+  { ssr: false },
+)
 
 export default AnimatedCursor
---
+++ src/lib/getDeviceType.ts
@@ -0,0 +1,7 @@
+export function getDeviceType(): string {
+  const userAgent = navigator.userAgent
+
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)
+    ? 'Mobile'
+    : 'Desktop'
+}
