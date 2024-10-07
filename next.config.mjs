c66874d68e3e95ff0f1718bbce660e4c8085eb26
johnlomat <edwardlomat1503@gmail.com>
2024-10-07 15:31:48 +0800

    Change domain

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'wordpress.gomasonboost.com',
+        hostname: 'wordpress.masonboost.com',
         port: '',
         pathname: '/wp-content/uploads/**',
       },
